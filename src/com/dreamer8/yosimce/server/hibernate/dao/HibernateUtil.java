0785f2e6c5a1a86c2faefe0208853a951c708892
Jorge Flores Ivelic <bestjef@gmail.com>
2013-07-12 02:56:53 -0400

    hibernate configurado pojos y xml creados

---
+++ src/com/dreamer8/yosimce/server/hibernate/dao/HibernateUtil.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dreamer8.yosimce.server.hibernate.dao;
+
+import java.net.SocketException;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.hibernate.SessionFactory;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author jorge
+ */
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    private static ServiceRegistry serviceRegistry;
+
+    static {
+	try {
+	    // Create the SessionFactory from standard (hibernate.cfg.xml) 
+	    // config file.
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	} catch (Throwable ex) {
+	    // Log the exception. 
+	    System.err.println("Initial SessionFactory creation failed." + ex);
+	    throw new ExceptionInInitializerError(ex);
+	}
+    }
+
+    public static SessionFactory getSessionFactory() {
+	return sessionFactory;
+    }
+
+    public static void resetSessionFactory() {
+	if (sessionFactory != null) {
+	    if (!sessionFactory.isClosed()) {
+		sessionFactory.close();
+	    }
+	}
+	try {
+	    // Create the SessionFactory from standard (hibernate.cfg.xml) 
+	    // config file.
+		Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	} catch (Throwable ex) {
+	    // Log the exception. 
+	    System.err.println("Initial SessionFactory creation failed." + ex);
+	    throw new ExceptionInInitializerError(ex);
+	}
+    }
+
+    public static void rollback(Session s) {
+	try {
+	    if (s.isOpen()) {
+		s.getTransaction().rollback();
+	    }
+	} catch (HibernateException ex) {
+//	    if (ex.indexOfThrowable(SocketException.class) != -1) {
+		resetSessionFactory();
+//	    }
+	}
+    }
+    public static void rollbackActiveOnly(Session s) {
+	try {
+	    if (s.isOpen()) {
+		if (s.getTransaction().isActive()) {
+		    s.getTransaction().rollback();
+		}
+	    }
+	} catch (HibernateException ex) {
+//	    if (ex.indexOfThrowable(SocketException.class) != -1) {
+		resetSessionFactory();
+//	    }
+	}
+    }
+}
