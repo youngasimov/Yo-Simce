96953278a9a9dccb55223649ddffa6f09ca6304e
Camilo Vera Cortés <cvera@dreamer8.com>
2013-07-23 17:20:41 -0400

    implementación de PlanificacionService

---
+++ src/com/dreamer8/yosimce/server/PlanificacionServiceImpl.java
@@ -0,0 +1,8 @@
+package com.dreamer8.yosimce.server;
+
+import com.dreamer8.yosimce.client.planificacion.PlanificacionService;
+
+public class PlanificacionServiceImpl extends CustomRemoteServiceServlet
+		implements PlanificacionService {
+
+}
