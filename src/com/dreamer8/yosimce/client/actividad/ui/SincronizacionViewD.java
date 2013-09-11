c5e7d89b358cf4814a3d5b4f523e43d28a16bc00
Camilo Vera Cortés <cvera@dreamer8.com>
2013-09-11 05:29:03 -0300

    se ordena la lista de alumnos

--- src/com/dreamer8/yosimce/client/actividad/ui/SincronizacionViewD.java
+++ src/com/dreamer8/yosimce/client/actividad/ui/SincronizacionViewD.java
@@ -1,6 +1,8 @@
 package com.dreamer8.yosimce.client.actividad.ui;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import com.dreamer8.yosimce.client.actividad.MaterialDefectuosoPlace;
 import com.dreamer8.yosimce.client.ui.OverMenuBar;
@@ -18,6 +20,7 @@ import com.google.gwt.cell.client.TextInputCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiFactory;
 import com.google.gwt.uibinder.client.UiField;
@@ -29,6 +32,8 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
+import com.google.gwt.view.client.ListDataProvider;
 
 public class SincronizacionViewD extends Composite implements
 		SincronizacionView {
@@ -64,6 +69,8 @@ public class SincronizacionViewD extends Composite implements
 	private FieldUpdater<SincAlumnoDTO, String> comentarioUpdater;
 	private FieldUpdater<SincAlumnoDTO, Boolean> formUpdater;
 	
+	private HandlerRegistration handlerRegistration;
+	private ListDataProvider<SincAlumnoDTO> dataProvider;
 	
 	private CursoDTO curso;
 
@@ -73,6 +80,8 @@ public class SincronizacionViewD extends Composite implements
 		dataGrid.setKeyboardPagingPolicy(KeyboardPagingPolicy.CURRENT_PAGE);
 		dataGrid.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.BOUND_TO_SELECTION);
 		estados = new ArrayList<EstadoSincronizacionDTO>();
+		dataProvider = new ListDataProvider<SincAlumnoDTO>(SincAlumnoDTO.KEY_PROVIDER);
+		dataProvider.addDataDisplay(dataGrid);
 		menu.insertSeparator(2);
 		menu.setOverItem(menuItem);
 		menu.setOverCommand(new Scheduler.ScheduledCommand() {
@@ -196,12 +205,83 @@ public class SincronizacionViewD extends Composite implements
 	@Override
 	public void setAlumnos(ArrayList<SincAlumnoDTO> alumnos) {
 		this.alumnos = alumnos;
+		Collections.sort(this.alumnos, new Comparator<SincAlumnoDTO>() {
+
+			@Override
+			public int compare(SincAlumnoDTO o1, SincAlumnoDTO o2) {
+				if(o1.getTipoAlumno() == null && o2.getTipoAlumno()!=null){
+					return 1;
+				}else if(o1.getTipoAlumno() != null && o2.getTipoAlumno()==null){
+					return -1;
+				}else if(o1.getTipoAlumno() != null && o2.getTipoAlumno()!=null && !o1.getTipoAlumno().equals(o2.getTipoAlumno())){
+					if(o1.getTipoAlumno().equalsIgnoreCase("Titular")){return -1;}
+					if(o1.getTipoAlumno().equalsIgnoreCase("Suplente") && !o1.getTipoAlumno().equalsIgnoreCase("Titular")){return -1;}
+					return o1.getTipoAlumno().compareTo(o2.getTipoAlumno());
+				}else if(!o1.getApellidoPaterno().equals(o2.getApellidoPaterno())){
+					return o1.getApellidoPaterno().compareToIgnoreCase(o2.getApellidoPaterno());
+				}else if(!o1.getApellidoMaterno().equals(o2.getApellidoMaterno())){
+					return o1.getApellidoMaterno().compareToIgnoreCase(o2.getApellidoMaterno());
+				}else {
+					return o1.getNombres().compareToIgnoreCase(o2.getNombres());
+				}
+			}
+		});
+		
 		if(!estados.isEmpty()){
+			dataProvider.setList(this.alumnos);
+			
+			if(handlerRegistration!=null){
+				handlerRegistration.removeHandler();
+			}
+			
+			
+			ListHandler<SincAlumnoDTO> h = new ListHandler<SincAlumnoDTO>(dataProvider.getList());
+			
+			handlerRegistration = dataGrid.addColumnSortHandler(h);
+			h.setComparator(dataGrid.getColumn(1), new Comparator<SincAlumnoDTO>() {
+				
+				@Override
+				public int compare(SincAlumnoDTO arg0, SincAlumnoDTO arg1) {
+					return arg0.getNombres().compareToIgnoreCase(arg1.getNombres());
+				}
+			});
+			
+			h.setComparator(dataGrid.getColumn(2), new Comparator<SincAlumnoDTO>() {
+				
+				@Override
+				public int compare(SincAlumnoDTO arg0, SincAlumnoDTO arg1) {
+					return arg0.getApellidoPaterno().compareToIgnoreCase(arg1.getApellidoPaterno());
+				}
+			});
+			
+			h.setComparator(dataGrid.getColumn(3), new Comparator<SincAlumnoDTO>() {
+				
+				@Override
+				public int compare(SincAlumnoDTO arg0, SincAlumnoDTO arg1) {
+					return arg0.getApellidoMaterno().compareToIgnoreCase(arg1.getApellidoMaterno());
+				}
+			});
+			
+			h.setComparator(dataGrid.getColumn(5), new Comparator<SincAlumnoDTO>() {
+				
+				@Override
+				public int compare(SincAlumnoDTO arg0, SincAlumnoDTO arg1) {
+					if(arg0.getTipoAlumno() == null){ return 1;} 
+					return arg0.getTipoAlumno().compareToIgnoreCase(arg1.getTipoAlumno());
+				}
+			});
+			
+			dataGrid.getColumnSortList().push(dataGrid.getColumn(4));
+			dataGrid.getColumnSortList().push(dataGrid.getColumn(1));
+			dataGrid.getColumnSortList().push(dataGrid.getColumn(3));
+			dataGrid.getColumnSortList().push(dataGrid.getColumn(2));
+			dataGrid.getColumnSortList().push(dataGrid.getColumn(5));
+			/*
 			dataGrid.setPageSize(alumnos.size()+1);
 			dataGrid.setPageStart(0);
 			dataGrid.setRowCount(alumnos.size());
 			dataGrid.setVisibleRange(0, alumnos.size());
-			dataGrid.setRowData(alumnos);
+			dataGrid.setRowData(this.alumnos);*/
 		}
 	}
 
@@ -238,7 +318,7 @@ public class SincronizacionViewD extends Composite implements
 				return ViewUtils.limitarString(o.getNombres(),20);
 			}
 		};
-		nombreColumn.setSortable(false);
+		nombreColumn.setSortable(true);
 		dataGrid.addColumn(nombreColumn, "Nombres");
 		dataGrid.setColumnWidth(nombreColumn, 160,Unit.PX);
 
@@ -250,7 +330,7 @@ public class SincronizacionViewD extends Composite implements
 				return (o.getApellidoPaterno()!=null)?o.getApellidoPaterno():"";
 			}
 		};
-		paternoColumn.setSortable(false);
+		paternoColumn.setSortable(true);
 		dataGrid.addColumn(paternoColumn, "A. Paterno");
 		dataGrid.setColumnWidth(paternoColumn,130,Unit.PX);
 
@@ -262,7 +342,7 @@ public class SincronizacionViewD extends Composite implements
 				return (o.getApellidoMaterno()!=null)?o.getApellidoMaterno():"";
 			}
 		};
-		maternoColumn.setSortable(false);
+		maternoColumn.setSortable(true);
 		dataGrid.addColumn(maternoColumn, "A. Materno");
 		dataGrid.setColumnWidth(maternoColumn,130,Unit.PX);
 
@@ -274,7 +354,7 @@ public class SincronizacionViewD extends Composite implements
 				return (o.getRut()!=null)?o.getRut():"";
 			}
 		};
-		rutColumn.setSortable(false);
+		rutColumn.setSortable(true);
 		dataGrid.addColumn(rutColumn, "RUT");
 		dataGrid.setColumnWidth(rutColumn,110,Unit.PX);
 		
@@ -285,7 +365,7 @@ public class SincronizacionViewD extends Composite implements
 				return (o.getTipoAlumno()!=null)?o.getTipoAlumno():"";
 			}
 		};
-		tipoColumn.setSortable(false);
+		tipoColumn.setSortable(true);
 		dataGrid.addColumn(tipoColumn, "Tipo alumno");
 		dataGrid.setColumnWidth(tipoColumn,130,Unit.PX);
 
