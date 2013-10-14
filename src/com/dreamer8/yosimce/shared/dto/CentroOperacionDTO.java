935dd0c614d472c631f96c708bd441a7f9720c93
Camilo Ignacio Vera Cortes <cvera@dreamer8.com>
2013-10-13 23:36:21 -0300

    centro operación DTO

--- src/com/dreamer8/yosimce/shared/dto/CentroOperacionDTO.java
+++ src/com/dreamer8/yosimce/shared/dto/CentroOperacionDTO.java
@@ -19,6 +19,15 @@ public class CentroOperacionDTO implements Serializable {
 	private Integer enEstablecimiento;
 	private Integer enImprenta;
 	private Integer enMinisterio;
+	private Integer contingenciaEnCentro;
+	private Integer contingenciaEnEstablecimiento;
+	private Integer contingenciaEnImprenta;
+	private Integer contingenciaEnMinisterio;
+	private Integer estadoSinInformacion;
+	private Integer estadoPorConfirmar;
+	private Integer estadoConfirmado;
+	private Integer estadoRealizado;
+	private Integer estadoAnulada;
 	
 	public CentroOperacionDTO() {
 		// TODO Auto-generated constructor stub
@@ -126,5 +135,78 @@ public class CentroOperacionDTO implements Serializable {
 
 	public void setEnMinisterio(Integer enMinisterio) {
 		this.enMinisterio = enMinisterio;
+	}
+
+	public Integer getContingenciaEnCentro() {
+		return contingenciaEnCentro;
+	}
+
+	public void setContingenciaEnCentro(Integer contingenciaEnCentro) {
+		this.contingenciaEnCentro = contingenciaEnCentro;
+	}
+
+	public Integer getContingenciaEnEstablecimiento() {
+		return contingenciaEnEstablecimiento;
+	}
+
+	public void setContingenciaEnEstablecimiento(
+			Integer contingenciaEnEstablecimiento) {
+		this.contingenciaEnEstablecimiento = contingenciaEnEstablecimiento;
+	}
+
+	public Integer getContingenciaEnImprenta() {
+		return contingenciaEnImprenta;
+	}
+
+	public void setContingenciaEnImprenta(Integer contingenciaEnImprenta) {
+		this.contingenciaEnImprenta = contingenciaEnImprenta;
+	}
+
+	public Integer getContingenciaEnMinisterio() {
+		return contingenciaEnMinisterio;
+	}
+
+	public void setContingenciaEnMinisterio(Integer contingenciaEnMinisterio) {
+		this.contingenciaEnMinisterio = contingenciaEnMinisterio;
+	}
+
+	public Integer getEstadoSinInformacion() {
+		return estadoSinInformacion;
+	}
+
+	public void setEstadoSinInformacion(Integer estadoSinInformacion) {
+		this.estadoSinInformacion = estadoSinInformacion;
+	}
+
+	public Integer getEstadoPorConfirmar() {
+		return estadoPorConfirmar;
+	}
+
+	public void setEstadoPorConfirmar(Integer estadoPorConfirmar) {
+		this.estadoPorConfirmar = estadoPorConfirmar;
+	}
+
+	public Integer getEstadoConfirmado() {
+		return estadoConfirmado;
+	}
+
+	public void setEstadoConfirmado(Integer estadoConfirmado) {
+		this.estadoConfirmado = estadoConfirmado;
+	}
+
+	public Integer getEstadoRealizado() {
+		return estadoRealizado;
+	}
+
+	public void setEstadoRealizado(Integer estadoRealizado) {
+		this.estadoRealizado = estadoRealizado;
+	}
+
+	public Integer getEstadoAnulada() {
+		return estadoAnulada;
+	}
+
+	public void setEstadoAnulada(Integer estadoAnulada) {
+		this.estadoAnulada = estadoAnulada;
 	}
 }
