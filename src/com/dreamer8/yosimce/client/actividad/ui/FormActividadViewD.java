42b7f6cd33fbd6eeb13cc56f9cc4b22c7a23f5a6
Camilo Vera Cortés <cvera@dreamer8.com>
2014-05-29 01:49:17 -0400

    se actualiza librería gwtUpload para dar soporte a gwt 2.6 e internet explorer 10

--- src/com/dreamer8/yosimce/client/actividad/ui/FormActividadViewD.java
+++ src/com/dreamer8/yosimce/client/actividad/ui/FormActividadViewD.java
@@ -219,7 +219,7 @@ public class FormActividadViewD extends Composite implements FormActividadView {
 				if(uploader.getStatus().equals(IUploadStatus.Status.SUCCESS)){
 					logger.log(Level.INFO, "Archivo uploaded success");
 					fileUploaded = true;
-					file = uploader.getBasename();
+					file = uploader.getFileInput().getFilename();
 					presenter.onDocumentoUploaded(file);
 				}
 			}
